9c7853ab866c88458129e8cbf15938a3d2a9b02b
ienesb <63236773+ienesb@users.noreply.github.com>
2020-05-10 22:51:16 +0300

    Update deneme.java

--- Deneme/deneme.java
+++ Deneme/deneme.java
@@ -4,6 +4,7 @@ public class deneme {
 	public static void main(String[] args) {
 		System.out.println("klsllfnldsdfkl");
 		System.out.println("bbbdeneme");
+		System.out.println("klsllfnldsdfkl");
 
 	}
 
